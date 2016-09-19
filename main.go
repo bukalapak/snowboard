9532c4d6cd0476ebbac14510388ff592a9da9633
Alif Rachmawadi <code@subosito.com>
2016-09-19 20:47:50 +0700

    added basic flag handler

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 /*
 #cgo CFLAGS: -I./ext/drafter/src/ -I./ext/drafter/ext/snowcrash/src/
 #cgo darwin LDFLAGS: -L"./ext/drafter/build/out/Release/" -ldrafter -lsos -lsnowcrash -lmarkdownparser -lsundown -lc++
@@ -11,10 +9,28 @@ import "fmt"
 #include "drafter.h"
 */
 import "C"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const appVersion = "0.1.0"
 
 func main() {
-	v := C.drafter_version_string()
-	s := C.GoString(v)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage:\n  snowboard [OPTIONS]\n\nOptions:\n")
+		flag.PrintDefaults()
+	}
 
-	fmt.Printf("Drafter version: %s\n", s)
+	version := flag.Bool("v", false, "Display version information")
+	flag.Parse()
+
+	if *version {
+		v := C.drafter_version_string()
+		s := C.GoString(v)
+		fmt.Printf("Snowboard version: v%s\n", appVersion)
+		fmt.Printf("Drafter version: %s\n", s)
+		os.Exit(0)
+	}
 }
