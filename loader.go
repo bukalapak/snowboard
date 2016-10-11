dd2c3a9bc0139c66ce4627413b0d446369cd4091
Alif Rachmawadi <code@subosito.com>
2016-10-11 09:18:02 +0700

    support html comment includes

--- loader.go
+++ loader.go
@@ -1,9 +1,13 @@
 package snowboard
 
 import (
+	"bufio"
 	"bytes"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"text/template"
 
 	"github.com/subosito/snowboard/api"
@@ -33,11 +37,34 @@ func (l *loader) partial(name string) string {
 	return string(b)
 }
 
+func partialComment(s string) string {
+	re := regexp.MustCompile(`<!--\s?include\((.+)\)\s?-->`)
+	rs := re.FindStringSubmatch(s)
+
+	if len(rs) != 2 {
+		return s
+	}
+
+	return `{{partial "` + rs[1] + `"}}`
+}
+
 // Load reads API Blueprint from file as blueprint.API struct using selected Parser
 func Load(name string, engine Parser) (*api.API, error) {
-	b, err := ioutil.ReadFile(name)
+	f, err := os.Open(name)
 	if err != nil {
 		return nil, err
+	}
+	defer f.Close()
+
+	cs := []string{}
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "<!--") {
+			cs = append(cs, partialComment(scanner.Text()))
+		} else {
+			cs = append(cs, scanner.Text())
+		}
 	}
 
 	loader := newLoader(name)
@@ -45,7 +72,9 @@ func Load(name string, engine Parser) (*api.API, error) {
 		"partial": loader.partial,
 	}
 
-	tmpl, err := template.New("apib").Funcs(funcMap).Parse(string(b))
+	s := strings.Join(cs, "\n")
+
+	tmpl, err := template.New("apib").Funcs(funcMap).Parse(s)
 	if err != nil {
 		return nil, err
 	}
