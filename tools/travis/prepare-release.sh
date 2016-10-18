724aea3f0431da04376c033d4a0abeb7fc4f7fd8
Alif Rachmawadi <code@subosito.com>
2016-10-18 08:59:39 +0700

    sha256 checksum

--- tools/travis/prepare-release.sh
+++ tools/travis/prepare-release.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+TRAVIS_ARCHIVE_FILE=snowboard-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar.gz
+
 if [[ "$TRAVIS_TEST_RESULT" == "0" ]]; then
-  echo "Generating snowboard-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar.gz"
+  echo "Generating ${TRAVIS_ARCHIVE_FILE}"
   make go-build
-  tar -czf $TRAVIS_BUILD_DIR/snowboard-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar.gz snowboard
+  tar -czf $TRAVIS_ARCHIVE_FILE snowboard
+  shasum -a 256 -b $TRAVIS_ARCHIVE_FILE
 fi
