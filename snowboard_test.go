6d576c2097b6c41869d8a8e4790072e324e13cc6
Alif Rachmawadi <code@subosito.com>
2017-01-07 20:42:59 +0700

    removed unused lines

--- snowboard_test.go
+++ snowboard_test.go
@@ -7,15 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/subosito/snowboard"
 	"github.com/subosito/snowboard/engines/drafter"
-	"github.com/subosito/snowboard/engines/drafterc"
 )
 
 func TestParse(t *testing.T) {
 	testParse(t, drafter.Engine{})
-}
-
-func TestParse_customEngine(t *testing.T) {
-	testParse(t, drafterc.Engine{})
 }
 
 func testParse(t *testing.T, parser snowboard.Parser) {
