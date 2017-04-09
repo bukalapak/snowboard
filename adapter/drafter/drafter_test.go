fec1eb6e3e34cd111b136c55e35ef91b53df84aa
Alif Rachmawadi <code@subosito.com>
2017-04-09 22:22:39 +0700

    updated drafter tests

--- adapter/drafter/drafter_test.go
+++ adapter/drafter/drafter_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/subosito/snowboard/drafter"
+	"github.com/subosito/snowboard/adapter/drafter"
 )
 
 func TestDrafter_Parse(t *testing.T) {
@@ -19,9 +19,20 @@ func TestDrafter_Parse(t *testing.T) {
 
 func TestDrafter_Validate(t *testing.T) {
 	c := drafter.Engine{}
-	s := strings.NewReader("# API\n## Data Structures\n### Hello-World (object)\n+ foo: bar (string, required)")
 
+	s := strings.NewReader("# API")
 	b, err := c.Validate(s)
 	assert.Nil(t, err)
+	assert.Empty(t, string(b))
+
+	s = strings.NewReader("# API\n## Data Structures\n### Hello-World (object)\n+ foo: bar (string, required)")
+	b, err = c.Validate(s)
+	assert.Nil(t, err)
 	assert.Contains(t, string(b), "please escape the name of the data structure using backticks")
+}
+
+func TestDrafter_Version(t *testing.T) {
+	c := drafter.Engine{}
+	v := c.Version()
+	assert.Equal(t, drafter.Version, v)
 }
