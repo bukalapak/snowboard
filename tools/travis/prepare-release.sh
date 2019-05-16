992d29f3503f8838cb8faf9f82c546430adcb759
Alif Rachmawadi <subosito@bukalapak.com>
2019-05-16 16:10:13 +0700

    updated travis release

--- tools/travis/prepare-release.sh
+++ tools/travis/prepare-release.sh
@@ -4,7 +4,13 @@ TRAVIS_ARCHIVE_FILE=snowboard-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar
 
 if [[ "$TRAVIS_TEST_RESULT" == "0" ]]; then
   echo "Generating ${TRAVIS_ARCHIVE_FILE}"
-  make go-build
-  tar -czf $TRAVIS_ARCHIVE_FILE snowboard
+
+  npm install -g pkg
+  pkg .
+
+  cp node_modules/protagonist/build/Release/protagonist.node .
+  cp snowboard-${TRAVIS_OS_NAME/osx/macos} snowboard
+
+  tar -czf $TRAVIS_ARCHIVE_FILE snowboard protagonist.node
   shasum -a 256 -b $TRAVIS_ARCHIVE_FILE
 fi
