f3313d783ec31f15f10cf9d69808cb8e8ca2bb45
Alif Rachmawadi <subosito@bukalapak.com>
2019-05-16 16:23:01 +0700

    updated examples

--- examples/generate.sh
+++ examples/generate.sh
@@ -2,11 +2,13 @@
 
 TEMPLATES=('alpha');
 
+git submodules update --init
+
 for template in $TEMPLATES; do
-  output_dir="$3/${template}"
+  output_dir="$2/${template}"
   mkdir -p "${output_dir}"
 
-  for example in ${2}/*.md; do
+  for example in ${1}/*.md; do
     fname="${example##*/}"
     fname_html="${fname/md/html}"
 
@@ -15,7 +17,7 @@ for template in $TEMPLATES; do
     fi
 
     echo "Generating ${fname}..."
-    "$1" html -t "${template}" -o "${output_dir}/${fname_html}" "${example}"
+    node lib/main.js html -o "${output_dir}/${fname_html}" "${example}"
     echo "Done!"
   done
 done
