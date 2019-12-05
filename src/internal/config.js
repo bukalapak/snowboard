fc2864ec7a8c6523e3966d19af1fc5e06a92b8f3
Alif Rachmawadi <subosito@bukalapak.com>
2019-12-05 08:49:52 +0700

    add config loader

---
+++ src/internal/config.js
@@ -0,0 +1,20 @@
+import cosmiconfig from "cosmiconfig";
+
+const moduleName = "snowboard";
+const explorer = cosmiconfig(moduleName, {
+  loaders: {
+    ".json": cosmiconfig.loadJson,
+    ".yaml": cosmiconfig.loadYaml,
+    ".yml": cosmiconfig.loadYaml
+  }
+});
+
+async function autoload(file) {
+  if (file) return explorer.load(file);
+  return explorer.search();
+}
+
+export default async function(file) {
+  const { config = {} } = await autoload(file);
+  return config;
+}
