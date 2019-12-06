266619d7f83801b9bf1c21256f9a92ccda6aa52f
Alif Rachmawadi <subosito@bukalapak.com>
2019-12-06 12:50:43 +0700

    add test for readPaths

---
+++ test/internal/input_test.js
@@ -0,0 +1,23 @@
+import test from "tape";
+import { isEqual } from "lodash";
+import { readPaths } from "../../src/internal/input";
+import { fixturePath } from "../helper";
+
+test("readPaths", async t => {
+  const prefix = fixturePath("partials/");
+  const paths = readPaths(fixturePath("partials/API.apib"));
+
+  const actualPaths = paths.map(path => path.replace(`${prefix}/`, ""));
+  const expectedPaths = [
+    "API.apib",
+    "messages/response/seed.json",
+    "messages/response/success.apib",
+    "messages/actions.apib",
+    "messages.apib",
+    "users.apib",
+    "tasks.apib"
+  ];
+
+  t.ok(isEqual(actualPaths, expectedPaths));
+  t.end();
+});
