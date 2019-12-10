af7947177a5146c7a67232daefa678eb4159e91b
Alif Rachmawadi <subosito@bukalapak.com>
2019-12-10 15:01:47 +0700

    update json subcommand

--- src/commands/json.js
+++ src/commands/json.js
@@ -1,16 +1,15 @@
 import { read } from "../internal/input";
 import { load } from "../parser";
-import { writeOutput } from "../internal/util";
+import { writeOutput, readAsElement, jsonStringify } from "../internal/util";
 
 async function jsonCmd(input, { output, quiet }) {
-  const source = await read(input);
-  const result = await load(source);
+  const result = await readAsElement(input);
 
   if (!result) {
     throw new Error(`ERROR: unable to parse input: ${input}`);
   }
 
-  writeOutput(output, JSON.stringify(result, null, 2));
+  writeOutput(output, jsonStringify(result, { optimized: false }));
 
   if (!quiet && output) {
     console.log("%s: API elements JSON has been generated", output);
