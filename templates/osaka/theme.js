ad402fceb7324579e0baee009a5554791a16db62
Alif Rachmawadi <subosito@bukalapak.com>
2019-12-20 10:03:35 +0700

    update osaka theme

--- templates/osaka/theme.js
+++ templates/osaka/theme.js
@@ -1,7 +1,16 @@
 import { theme } from "@chakra-ui/core";
 
 const customTheme = {
-  ...theme
+  ...theme,
+  colors: {
+    ...theme.colors,
+    dark: "#1a202c",
+    light: "#fff"
+  },
+  fontSizes: {
+    ...theme.fontSizes,
+    "2xs": "10px"
+  }
 };
 
 export default customTheme;
