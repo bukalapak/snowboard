ae8c60df540b30f30f4582355f40d5d13dce96ad
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-07 22:30:57 +0700

    safe json parse for json tree

--- templates/osaka/components/JSONTree.js
+++ templates/osaka/components/JSONTree.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactJson from "react-json-view";
+import JSONParse from "json-parse-safe";
 import { Code } from "@chakra-ui/core";
 
 export default function JSONTree({ src, contentType = "" }) {
   if (contentType.match(/json/)) {
+    const { value } = JSONParse(src);
+
+    if (!value) {
+      return <PlainCode src={src} />;
+    }
+
     return (
       <ReactJson
-        src={JSON.parse(src)}
+        src={value}
         enableClipboard={false}
         theme="hopscotch"
         name={false}
@@ -23,6 +30,10 @@ export default function JSONTree({ src, contentType = "" }) {
     );
   }
 
+  return <PlainCode src={src} />;
+}
+
+function PlainCode({ src }) {
   return (
     <Code whiteSpace="pre-wrap" px="6" py="4" w="full">
       {src}
