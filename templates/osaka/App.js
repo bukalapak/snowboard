6bb51d0e9347086c983d6c81ea5b664185ca72b9
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-07 22:30:01 +0700

    use helmet and store provider

--- templates/osaka/App.js
+++ templates/osaka/App.js
@@ -2,26 +2,32 @@ import React, { Suspense } from "react";
 import { Router, View } from "react-navi";
 import { ThemeProvider } from "emotion-theming";
 import { ColorModeProvider, useColorMode } from "@chakra-ui/core";
+import HelmetProvider from "react-navi-helmet-async";
 import { CSSReset } from "@chakra-ui/core";
 import { ConfigProvider } from "./lib/hooks/config";
+import { StoreProvider } from "./lib/hooks/store";
 import routes from "./routes";
 import theme from "./theme";
 import config from "./config";
 
 const App = () => {
   return (
-    <Router routes={routes}>
-      <ConfigProvider config={config}>
-        <ThemeProvider theme={theme}>
-          <CSSReset />
-          <ColorModeProvider>
-            <Suspense fallback={null}>
-              <View />
-            </Suspense>
-          </ColorModeProvider>
-        </ThemeProvider>
-      </ConfigProvider>
-    </Router>
+    <HelmetProvider>
+      <Router routes={routes}>
+        <ConfigProvider config={config}>
+          <StoreProvider>
+            <ThemeProvider theme={theme}>
+              <CSSReset />
+              <ColorModeProvider>
+                <Suspense fallback={null}>
+                  <View />
+                </Suspense>
+              </ColorModeProvider>
+            </ThemeProvider>
+          </StoreProvider>
+        </ConfigProvider>
+      </Router>
+    </HelmetProvider>
   );
 };
 
