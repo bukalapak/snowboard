a25ac54b19b8ac44376c1226e0493b7fa2b34bfa
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-16 21:20:53 +0700

    update json command

--- src/commands/json.js
+++ src/commands/json.js
@@ -1,19 +1,42 @@
-import { read } from "../internal/input";
-import { load } from "../parser";
-import { writeOutput, readAsElement, jsonStringify } from "../internal/util";
+import { Command, flags } from "@oclif/command";
+import { readAsElement, jsonStringify } from "../util";
 
-async function jsonCmd(input, { output, quiet }) {
-  const result = await readAsElement(input);
+class JsonCommand extends Command {
+  static args = [{ name: "input", required: true }];
 
-  if (!result) {
-    throw new Error(`ERROR: unable to parse input: ${input}`);
-  }
+  async run() {
+    const { flags, args } = this.parse(JsonCommand);
 
-  writeOutput(output, jsonStringify(result, { optimized: false }));
+    if (!flags.output && flags.quiet) {
+      this.error("--quiet cannot be used when no --output=");
+    }
 
-  if (!quiet && output) {
-    console.log("%s: API elements JSON has been generated", output);
+    const result = await readAsElement(args.input);
+
+    if (!result) {
+      this.error(`unable to parse input: ${args.input}`);
+    }
+
+    const data = jsonStringify(result, { optimized: flags.optimized });
+
+    if (flags.output) {
+      await writeFile(flags.output, data);
+    } else {
+      this.log(data);
+    }
+
+    if (!flags.quiet && flags.output) {
+      this.log("%s: API elements JSON has been saved", flags.output);
+    }
   }
 }
 
-export default jsonCmd;
+JsonCommand.description = `render API elements json`;
+
+JsonCommand.flags = {
+  output: flags.string({ char: "o", description: "output file" }),
+  optimized: flags.boolean({ char: "O", description: "optimized mode" }),
+  quiet: flags.boolean({ char: "q", description: "quiet mode" })
+};
+
+export default JsonCommand;
