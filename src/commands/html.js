361cae3271510e46988ce834d11a6d6313750966
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-17 05:36:56 +0700

    update html command

--- src/commands/html.js
+++ src/commands/html.js
@@ -1,5 +1,32 @@
-import { htmlBundle } from "./bundle";
+import { Command, flags } from "@oclif/command";
+import { htmlBundle } from "../bundle";
 
-export default async function htmlCmd(input, cmd, commander) {
-  await htmlBundle(input, cmd, { watch: !!commander.watch });
+class HtmlCommand extends Command {
+  static args = [{ name: "input", required: true }];
+
+  async run() {
+    const { flags, args } = this.parse(HtmlCommand);
+
+    await htmlBundle(args.input, {
+      watch: flags.watch,
+      output: flags.output,
+      template: flags.template,
+      optimized: flags.optimized
+    });
+  }
 }
+
+HtmlCommand.description = `render HTML documentation`;
+
+HtmlCommand.flags = {
+  output: flags.string({ char: "o", description: "output directory" }),
+  template: flags.string({ char: "t", description: "custom template" }),
+  optimized: flags.boolean({ char: "O", description: "optimized mode" }),
+  watch: flags.boolean({
+    char: "w",
+    description: "watch for the files changes"
+  }),
+  quiet: flags.boolean({ char: "q", description: "quiet mode" })
+};
+
+export default HtmlCommand;
