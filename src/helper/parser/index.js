c0578a6df0fe6110312d358a82b420d43920ccde
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-20 20:29:40 +0700

    move utils to helper: parser

---
+++ src/helper/parser/index.js
@@ -0,0 +1,11 @@
+export { readAsRefract, readAsElement, readMultiAsElement } from "./reader";
+export { toValue, toDescription, toHeader, toParameters } from "./transform";
+export { resourceTitle, transitionTitle } from "./transform";
+export { transitionPath, transformPath } from "./transform";
+export {
+  groupPermalink,
+  resourcePermalink,
+  transitionPermalink
+} from "./permalink";
+export { digParameters } from "./digger";
+export { expand, normalizeStructures } from "./expand";
