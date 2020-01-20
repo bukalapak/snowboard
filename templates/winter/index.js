aed191d0ec210671baaf80d0a0941af5aa1596dc
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-20 12:37:53 +0700

    use seeds for winter

--- templates/winter/index.js
+++ templates/winter/index.js
@@ -1,8 +1,9 @@
 import App from "./App.svelte";
+import seeds from "./seeds";
 
 const app = new App({
   target: document.getElementById("root"),
-  props: {}
+  props: seeds
 });
 
 export default app;
