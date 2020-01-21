4b04e17633f8dbf6aaa2e50e67420b5215c6382c
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-21 15:24:12 +0700

    update built-in theme

--- templates/osaka/theme.js
+++ templates/osaka/theme.js
@@ -1,16 +1,1 @@
-import { theme } from "@chakra-ui/core";
-
-const customTheme = {
-  ...theme,
-  colors: {
-    ...theme.colors,
-    dark: "#1a202c",
-    light: "#fff"
-  },
-  fontSizes: {
-    ...theme.fontSizes,
-    "2xs": "10px"
-  }
-};
-
-export default customTheme;
+export { LightTheme, DarkTheme } from "baseui";
