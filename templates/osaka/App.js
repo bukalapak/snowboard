9421197dfc57b1722cc7ae8c593a5f41e273216c
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-27 21:36:38 +0700

    add store provider

--- templates/osaka/App.js
+++ templates/osaka/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Router, View, NotFoundBoundary } from "react-navi";
 import HelmetProvider from "react-navi-helmet-async";
 import { ThemeProvider } from "./lib/theme";
+import { StoreProvider } from "./lib/store";
 import routes from "./routes";
 import seeds from "./seeds";
 
@@ -10,9 +11,11 @@ const App = () => {
     <HelmetProvider>
       <Router routes={routes} context={seeds}>
         <ThemeProvider>
-          <Suspense fallback={null}>
-            <View />
-          </Suspense>
+          <StoreProvider>
+            <Suspense fallback={null}>
+              <View />
+            </Suspense>
+          </StoreProvider>
         </ThemeProvider>
       </Router>
     </HelmetProvider>
