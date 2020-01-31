a6c09f9a3a8cb76fcdfce3298118112ca441281f
Alif Rachmawadi <subosito@bukalapak.com>
2020-01-31 09:39:02 +0700

    restructure files

--- src/internal/config.js
+++
@@ -1,9 +0,0 @@
-import { cosmiconfig } from "cosmiconfig";
-
-const moduleName = "snowboard";
-const explorer = cosmiconfig(moduleName);
-
-export default async function() {
-  const { config } = (await explorer.search()) || { config: {} };
-  return config;
-}
