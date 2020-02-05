b8fd8f1b1ae5776e4d4a20d7c254d4cc27f57541
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 09:39:06 +0700

    add snowboard apib subcommand

---
+++ packages/snowboard/src/commands/apib.js
@@ -0,0 +1,36 @@
+import { Command, flags } from "@oclif/command";
+import { read } from "snowboard-reader";
+import { writeFile } from "snowboard-helper";
+
+class ApibCommand extends Command {
+  static args = [{ name: "input", required: true }];
+
+  async run() {
+    const { flags, args } = this.parse(ApibCommand);
+
+    if (!flags.output && flags.quiet) {
+      this.error("--quiet cannot be used when no --output=");
+    }
+
+    const source = await read(args.input);
+
+    if (flags.output) {
+      await writeFile(flags.output, source);
+    } else {
+      this.log(source);
+    }
+
+    if (!flags.quiet && flags.output) {
+      this.log("%s: API blueprint has been saved", flags.output);
+    }
+  }
+}
+
+ApibCommand.description = `render API blueprint`;
+
+ApibCommand.flags = {
+  output: flags.string({ char: "o", description: "output file" }),
+  quiet: flags.boolean({ char: "q", description: "quiet mode" })
+};
+
+export default ApibCommand;
