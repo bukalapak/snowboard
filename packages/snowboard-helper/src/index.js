9fe6dc9612194a3bfa4ffccdf7255efd5f36de8f
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 09:27:35 +0700

    add helper functions

---
+++ packages/snowboard-helper/src/index.js
@@ -0,0 +1,30 @@
+import {
+  writeFile as fsWriteFile,
+  readFile as fsReadFile,
+  existsSync as exists
+} from "fs";
+import { promisify } from "util";
+import { mkdirp, copy as cp, remove as rm } from "fs-extra";
+import tmp from "tmp";
+import uuidv4 from "uuid/v4";
+import safeStringify from "safe-json-stringify";
+
+export { table, borderlessTable, spinner } from "./render";
+export { toValue, toPath, transitionHref } from "./parser";
+
+export { toSlug } from "snowboard-theme-helper";
+
+export { mkdirp, exists, cp, rm };
+
+export const writeFile = promisify(fsWriteFile);
+export const readFile = promisify(fsReadFile);
+export const tmpdir = promisify(tmp.dir);
+
+export function uuid() {
+  return uuidv4();
+}
+
+export function jsonStringify(data, compact = false) {
+  const space = compact ? 0 : 2;
+  return safeStringify(data, null, space);
+}
