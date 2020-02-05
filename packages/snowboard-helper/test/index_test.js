691b76eea89bcc4fa75448aacd43bfc29ad75f5d
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 09:27:50 +0700

    add test for toSlug helper

---
+++ packages/snowboard-helper/test/index_test.js
@@ -0,0 +1,8 @@
+import test from "tape";
+import { toSlug } from "../src";
+
+test("toSlug", t => {
+  t.equal(toSlug("Hello World!"), "hello~world");
+  t.equal(toSlug("Hello World!", "/"), "hello/world");
+  t.end();
+});
