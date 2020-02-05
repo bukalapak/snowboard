64bf0d82f6c10c126dc24e476bce47553b927946
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 09:20:03 +0700

    add mock router parser

---
+++ packages/snowboard-mock-router/src/parser.js
@@ -0,0 +1,46 @@
+import { toValue, toPath, transitionHref } from "snowboard-helper";
+
+export function parse(element) {
+  const transitions = [];
+
+  element.api.resourceGroups.forEach(group => {
+    group.resources.forEach(resource => {
+      transitions.push(...extract(resource));
+    });
+  });
+
+  element.api.resources.forEach(resource => {
+    transitions.push(...extract(resource));
+  });
+
+  return transitions;
+}
+
+function extract(resource) {
+  const data = [];
+
+  resource.transitions.forEach(transition => {
+    const path = transitionHref(transition, resource);
+
+    data.push({
+      method: toValue(transition.method),
+      path: toPath(path),
+      requests: transition.transactions.map(({ request }) => {
+        return {
+          title: toValue(request.title),
+          method: toValue(request.method),
+          contentType: toValue(request.contentType)
+        };
+      }),
+      responses: transition.transactions.map(({ response }) => {
+        return {
+          statusCode: toValue(response.statusCode),
+          contentType: toValue(response.contentType),
+          body: toValue(response.messageBody)
+        };
+      })
+    });
+  });
+
+  return data;
+}
