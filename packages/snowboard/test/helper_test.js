d5585290fd32ed4bfb59f0319598e64aba1770d7
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 18:24:06 +0700

    add test for parseBinding

---
+++ packages/snowboard/test/helper_test.js
@@ -0,0 +1,8 @@
+import test from "tape";
+import { parseBinding } from "../src/helper";
+
+test("parseBinding", t => {
+  t.deepEqual(parseBinding(":8087"), [undefined, 8087]);
+  t.deepEqual(parseBinding("localhost:8088"), ["localhost", 8088]);
+  t.end();
+});
