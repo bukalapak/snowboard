fd86ac3a8fddcb66508238a227ac158b9ca0c0eb
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 10:25:29 +0700

    add list parser

---
+++ packages/snowboard/src/parser/index.js
@@ -0,0 +1,34 @@
+import { uniq } from "lodash";
+import { toValue, toPath, transitionHref } from "snowboard-helper";
+
+export function list(element) {
+  const data = [];
+
+  element.api.resourceGroups.forEach(group => {
+    group.resources.forEach(resource => {
+      data.push(...listExtract(resource));
+    });
+  });
+
+  element.api.resources.forEach(resource => {
+    data.push(...listExtract(resource));
+  });
+
+  return data;
+}
+
+function listExtract(resource) {
+  return resource.transitions.map(transition => {
+    const href = transitionHref(transition, resource);
+
+    return {
+      method: toValue(transition.method),
+      path: toPath(href),
+      statusCode: uniq(
+        transition.transactions.map(({ response }) =>
+          toValue(response.statusCode)
+        )
+      )
+    };
+  });
+}
