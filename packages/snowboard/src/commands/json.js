afe86b1f1743ee531928b22c791d3ed2f16bbad3
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-05 09:43:44 +0700

    add snowboard json subcommand

---
+++ packages/snowboard/src/commands/json.js
@@ -0,0 +1,43 @@
+import { Command, flags } from "@oclif/command";
+import { jsonStringify } from "snowboard-helper";
+import { readAsElement } from "../helper";
+
+class JsonCommand extends Command {
+  static args = [{ name: "input", required: true }];
+
+  async run() {
+    const { flags, args } = this.parse(JsonCommand);
+
+    if (!flags.output && flags.quiet) {
+      this.error("--quiet cannot be used when no --output=");
+    }
+
+    const result = await readAsElement(args.input);
+
+    if (!result) {
+      this.error(`unable to parse input: ${args.input}`);
+    }
+
+    const data = jsonStringify(result, flags.optimized);
+
+    if (flags.output) {
+      await writeFile(flags.output, data);
+    } else {
+      this.log(data);
+    }
+
+    if (!flags.quiet && flags.output) {
+      this.log("%s: API elements JSON has been saved", flags.output);
+    }
+  }
+}
+
+JsonCommand.description = `render API elements json`;
+
+JsonCommand.flags = {
+  output: flags.string({ char: "o", description: "output file" }),
+  optimized: flags.boolean({ char: "O", description: "optimized mode" }),
+  quiet: flags.boolean({ char: "q", description: "quiet mode" })
+};
+
+export default JsonCommand;
