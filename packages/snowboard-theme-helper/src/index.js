5b8aabc40fea6d40637501f599fcd134eb1c51c3
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-06 10:15:36 +0700

    expose store helper

--- packages/snowboard-theme-helper/src/index.js
+++ packages/snowboard-theme-helper/src/index.js
@@ -6,6 +6,21 @@ export { toNavigation, filterNavigation } from "./navigation";
 export { toTransactions } from "./transactions";
 export { exchangeToken, exchangeTokenWithPKCE, sendRequest } from "./oauth2";
 
+export {
+  getPKCE,
+  getState,
+  clearPKCE,
+  clearState,
+  setEnv,
+  getEnv,
+  setToken,
+  getToken,
+  removeToken,
+  setRefreshToken,
+  getRefreshToken,
+  removeRefreshToken
+} from "./store";
+
 export const jsonParse = jsonParseSafe;
 export const clipboardCopy = clipboardCopyOriginal;
 
