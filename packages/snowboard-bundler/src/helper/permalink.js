0eb063a5b890646fa84eab7580e7eb607f509462
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-06 08:53:16 +0700

    add permalink helpers

---
+++ packages/snowboard-bundler/src/helper/permalink.js
@@ -0,0 +1,42 @@
+import { toSlug, toValue, toPath, transitionHref } from "snowboard-helper";
+import { resourceTitle } from "./transform";
+
+const permalinkPrefix = {
+  group: "g",
+  resource: "r",
+  transition: "t"
+};
+
+export function groupPermalink(group) {
+  const title = toValue(group.title);
+  return toSlug(`${permalinkPrefix.group} ${title}`);
+}
+
+export function resourcePermalink(resource, group) {
+  if (group) {
+    const groupTitle = toValue(group.title);
+    const title = resourceTitle(resource);
+    return toSlug(`${permalinkPrefix.resource} ${groupTitle} ${title}`);
+  }
+
+  const title = resourceTitle(resource);
+  return toSlug(`${permalinkPrefix.resource} ${title}`);
+}
+
+export function transitionPermalink(transition, resource, group) {
+  const method = toValue(transition.method);
+  const path = transitionHref(transition, resource);
+  const pathTransformed = toPath(path);
+  const resTitle = resourceTitle(resource);
+
+  if (group) {
+    const groupTitle = toValue(group.title);
+    return toSlug(
+      `${permalinkPrefix.transition} ${groupTitle} ${resTitle} ${method} ${pathTransformed}`
+    );
+  }
+
+  return toSlug(
+    `${permalinkPrefix.transition} ${resTitle} ${method} ${pathTransformed}`
+  );
+}
