a6bb62804357d2ff001a76d3b9fa9965800b9848
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:20:17 +0700

    winter: add response panel

---
+++ packages/snowboard-theme-winter/winter/components/ResponsePanel.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { markdown } from "../lib/helper";
+  import HeaderTable from "../components/HeaderTable.svelte";
+  import CodePanel from "../components/CodePanel.svelte";
+
+  export let response;
+  export let show;
+</script>
+
+<style>
+  .is-borderless {
+    box-shadow: none;
+  }
+
+  .is-bordered {
+    border-top: solid 1px rgba(10, 10, 10, 0.1);
+    border-bottom: solid 1px rgba(10, 10, 10, 0.1);
+    background-color: rgba(10, 10, 10, 0.035);
+  }
+</style>
+
+<div class="card is-borderless">
+  <header class="card-header is-borderless">
+    <p class="card-header-title">
+      {#if response.title}
+        Response {response.title}
+      {:else}Response {response.statusCode}{/if}
+    </p>
+
+    <a
+      href="javascript:void(0)"
+      class="card-header-icon"
+      on:click={() => (show = !show)}>
+      <span class="icon">
+        <i
+          class="fas"
+          class:fa-angle-down={show}
+          class:fa-angle-up={!show}
+          aria-hidden="true" />
+      </span>
+    </a>
+  </header>
+  <div class="card-content is-bordered" class:is-hidden={!show}>
+    {#if response.description}
+      <div class="content">
+        {@html markdown(response.description)}
+      </div>
+    {/if}
+
+    <HeaderTable headers={response.headers} />
+
+    <CodePanel
+      contentType={response.contentType}
+      body={response.body}
+      schema={response.schema} />
+  </div>
+</div>
