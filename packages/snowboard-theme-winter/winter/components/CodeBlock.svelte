f23f61b93312d7dce416fb5c88513aa3a058365e
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:18:36 +0700

    winter: add code panel component

---
+++ packages/snowboard-theme-winter/winter/components/CodeBlock.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { highlight, stringify } from "../lib/helper";
+
+  export let type;
+  export let body;
+
+  const alias = str => {
+    return str && str.match("json") ? "json" : "markup";
+  };
+
+  $: mime = alias(type);
+</script>
+
+{#if body}
+  <pre class="language-{mime}">
+    <code class="language-{mime}">
+      {@html highlight(stringify(body), mime)}
+    </code>
+  </pre>
+{/if}
