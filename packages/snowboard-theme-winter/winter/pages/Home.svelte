2bbff915a058cc09cdd3df9c742b0e10371c120f
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:20:30 +0700

    winter: add home page

---
+++ packages/snowboard-theme-winter/winter/pages/Home.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { markdown } from "../lib/helper";
+
+  export let title;
+  export let description;
+</script>
+
+<h1 class="title">{title}</h1>
+<div class="content">
+  {@html markdown(description)}
+</div>
