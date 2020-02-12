54537b3dc39228434589603e8cbbb233b2fb6588
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:20:48 +0700

    winter: add group page

---
+++ packages/snowboard-theme-winter/winter/pages/Group.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { markdown } from "../lib/helper";
+  import Breadcrumb from "../components/Breadcrumb.svelte";
+
+  export let group;
+</script>
+
+<Breadcrumb {group} />
+
+<h1 class="title">{group.title}</h1>
+
+<hr />
+
+<div class="content">
+  {@html markdown(group.description)}
+</div>
