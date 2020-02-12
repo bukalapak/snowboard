bffd8a76652248fa845d5de946d57bb44b440210
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:16:04 +0700

    add winter markdown helper

---
+++ packages/snowboard-theme-winter/winter/lib/helper/markdown.js
@@ -0,0 +1,18 @@
+import marked from "marked";
+import highlight from "./highlight";
+
+marked.setOptions({
+  highlight
+});
+
+const renderer = new marked.Renderer();
+
+renderer.pre = renderer.code;
+renderer.code = function(code, infostring, escaped) {
+  const out = this.pre(code, infostring, escaped);
+  return out.replace("<pre>", `<pre class="language-${infostring}">`);
+};
+
+export default function(source) {
+  return source ? marked(source, { renderer: renderer }) : "";
+}
