7adbbe3b2210863cc8e458292925d06eebbecc09
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:15:55 +0700

    add winter highlight helper

---
+++ packages/snowboard-theme-winter/winter/lib/helper/highlight.js
@@ -0,0 +1,31 @@
+import Prism from "prismjs";
+
+Prism.languages.json = {
+  property: {
+    pattern: /"(?:\\.|[^\\"\r\n])*"(?=\s*:)/,
+    greedy: true
+  },
+  string: {
+    pattern: /"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
+    greedy: true
+  },
+  comment: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,
+  number: /-?\d+\.?\d*(e[+-]?\d+)?/i,
+  punctuation: /[{}[\],]/,
+  operator: /:/,
+  boolean: /\b(?:true|false)\b/,
+  null: {
+    pattern: /\bnull\b/,
+    alias: "keyword"
+  }
+};
+
+export default function(code, lang) {
+  const supported = ["xml", "json"];
+
+  if (!supported.includes(lang)) {
+    lang = "markup";
+  }
+
+  return Prism.highlight(code, Prism.languages[lang], lang);
+}
