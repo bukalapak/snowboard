1d7faeb4056d7b4cb9b8f06f6e2cd6ee602d0a13
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:20:59 +0700

    winter: add resource page

---
+++ packages/snowboard-theme-winter/winter/pages/Resource.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { markdown } from "../lib/helper";
+  import Breadcrumb from "../components/Breadcrumb.svelte";
+
+  export let group;
+  export let resource;
+</script>
+
+<Breadcrumb {group} {resource} />
+
+<h1 class="title">{resource.title}</h1>
+
+<hr />
+
+<div class="content">
+  {@html markdown(resource.description)}
+</div>
