89c89153235355caee41788c662dac28f6892d27
Alif Rachmawadi <subosito@bukalapak.com>
2020-02-12 23:18:05 +0700

    winter: add breadcrumb component

---
+++ packages/snowboard-theme-winter/winter/components/Breadcrumb.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { Link } from "yrv";
+  import { toHref } from "../lib/helper";
+
+  export let group;
+  export let resource;
+  export let transition;
+</script>
+
+{#if resource || transition}
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      {#if group}
+        <li>
+          <Link href={toHref(group.permalink)}>{group.title}</Link>
+        </li>
+      {/if}
+
+      {#if resource}
+        <li>
+          <Link href={toHref(resource.permalink)}>{resource.title}</Link>
+        </li>
+      {/if}
+
+      {#if transition}
+        <li>
+          <Link href={toHref(transition.permalink)}>{transition.title}</Link>
+        </li>
+      {/if}
+    </ul>
+  </nav>
+{/if}
