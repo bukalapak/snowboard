ca428e0781896a487b80a60b37d3bd32435fffce
Alif Rachmawadi <subosito@bukalapak.com>
2020-03-03 13:47:51 +0700

    add toCurl helper

--- packages/snowboard-theme-helper/src/request.js
+++ packages/snowboard-theme-helper/src/request.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import uriTemplate from "uritemplate";
 import urlParse from "url-parse";
+import r2curl from "r2curl";
+import urlJoin from "url-join";
+import qs from "querystringify";
 
 const expandUrl = (uri, obj) => {
   const tpl = uriTemplate.parse(uri);
@@ -34,4 +37,28 @@ const sendRequest = ({
   return client.request(options);
 };
 
-export { urlParse, expandUrl, sendRequest };
+const toCurl = (
+  { environment, method, body, headers, parameters, pathTemplate }
+) => {
+  const expandedUrl = expandUrl(pathTemplate, parameters);
+  const destUrl = urlParse(expandedUrl, true);
+
+  return r2curl({
+    url:
+      urlJoin(environment.url, destUrl.pathname) +
+      qs.stringify(destUrl.query, true),
+    method: method,
+    data: safeParse(body),
+    headers: headers
+  });
+};
+
+function safeParse(str) {
+  try {
+    return JSON.parse(str);
+  } catch (err) {
+    return str;
+  }
+}
+
+export { urlParse, expandUrl, sendRequest, toCurl };
