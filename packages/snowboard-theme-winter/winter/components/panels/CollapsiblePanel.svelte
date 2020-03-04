3816f34675955485e8048dc816629d00065eb459
Alif Rachmawadi <subosito@bukalapak.com>
2020-03-04 15:06:08 +0700

    add collapsible panel component

---
+++ packages/snowboard-theme-winter/winter/components/panels/CollapsiblePanel.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { darkMode } from "../../lib/store";
+  import ToggleButton from "../buttons/ToggleButton.svelte";
+
+  export let show = false;
+</script>
+
+<style>
+  .panel-section {
+    padding: 1em;
+  }
+
+  .panel-button {
+    border-radius: 4px;
+  }
+
+  .is-darkmode .panel-heading {
+    border: 1px solid #363636;
+  }
+
+  .is-darkmode .panel-section {
+    background-color: #222 !important;
+    border-color: #333;
+  }
+</style>
+
+<div class="panel" class:is-darkmode={$darkMode}>
+  <div
+    class="panel-heading"
+    class:has-background-dark={$darkMode}
+    class:has-text-white={$darkMode}
+    class:panel-button={!show}>
+    <slot name="heading" />
+    <ToggleButton
+      {show}
+      additionalClass={$darkMode ? 'has-text-grey is-pulled-right' : 'is-pulled-right'}
+      handleClick={() => (show = !show)} />
+  </div>
+  <div class="panel-section" class:is-hidden={!show}>
+    <slot name="body" />
+  </div>
+</div>
