be4d4db28ed124c91ec4ce80a1900e2a3af05224
Alif Rachmawadi <subosito@bukalapak.com>
2020-03-04 15:06:30 +0700

    add toggle button component

---
+++ packages/snowboard-theme-winter/winter/components/buttons/ToggleButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let show = false;
+  export let additionalClass = "";
+
+  export let handleClick;
+
+  function toggle(event) {
+    show = !show;
+    handleClick(event);
+  }
+</script>
+
+<style>
+  .toggle-icon {
+    cursor: pointer;
+  }
+</style>
+
+<span class="toggle-icon icon {additionalClass}" on:click={toggle}>
+  <i class="fas" class:fa-chevron-up={!show} class:fa-chevron-down={show} />
+</span>
