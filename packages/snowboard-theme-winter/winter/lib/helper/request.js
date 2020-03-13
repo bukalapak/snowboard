fd3b1b5cc008978cf0de823f1f095351ee6955bc
Alif Rachmawadi <subosito@bukalapak.com>
2020-03-13 12:42:22 +0700

    use httpClient from helper

--- packages/snowboard-theme-winter/winter/lib/helper/request.js
+++ packages/snowboard-theme-winter/winter/lib/helper/request.js
@@ -1,1 +1,23 @@
-export { sendRequest } from "snowboard-theme-helper";
+import { httpClient } from "snowboard-theme-helper";
+
+export const sendRequest = ({
+  environment,
+  method,
+  pathTemplate,
+  headers,
+  parameters,
+  body,
+  httpConfig
+}) => {
+  const [client, options] = httpClient({
+    environment,
+    method,
+    pathTemplate,
+    headers,
+    parameters,
+    body,
+    httpConfig
+  });
+
+  return client.request(options);
+};
