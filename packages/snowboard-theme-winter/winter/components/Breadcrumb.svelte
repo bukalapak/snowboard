d5df50e7dcbf36edf6b39a4bb83aaeb1d63aeed3
Alif Rachmawadi <subosito@bukalapak.com>
2020-04-09 00:46:03 +0700

    add support custom basepath for breadcrumb

--- packages/snowboard-theme-winter/winter/components/Breadcrumb.svelte
+++ packages/snowboard-theme-winter/winter/components/Breadcrumb.svelte
@@ -3,6 +3,7 @@
   import { toHref } from "../lib/helper";
 
   export let group;
+  export let config;
   export let resource = undefined;
   export let transition = undefined;
 </script>
@@ -12,19 +13,19 @@
     <ul>
       {#if group}
         <li>
-          <Link href={toHref(group.permalink)}>{group.title}</Link>
+          <Link href={toHref(group.permalink, config.basePath)}>{group.title}</Link>
         </li>
       {/if}
 
       {#if resource}
         <li>
-          <Link href={toHref(resource.permalink)}>{resource.title}</Link>
+          <Link href={toHref(resource.permalink, config.basePath)}>{resource.title}</Link>
         </li>
       {/if}
 
       {#if transition}
         <li>
-          <Link href={toHref(transition.permalink)}>{transition.title}</Link>
+          <Link href={toHref(transition.permalink, config.basePath)}>{transition.title}</Link>
         </li>
       {/if}
     </ul>
