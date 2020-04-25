6c43294b43ccb2294e47cd771e6b864fd0a05dc9
Alif Rachmawadi <subosito@bukalapak.com>
2020-04-25 12:35:21 +0700

    add osaka header table component

---
+++ packages/snowboard-theme-osaka/osaka/components/HeaderTable.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Table } from "baseui/table";
+
+export default function({ headers }) {
+  return (
+    <Table
+      columns={["Headers"]}
+      data={headers.map(header => [header.name, header.example])}
+    />
+  );
+}
