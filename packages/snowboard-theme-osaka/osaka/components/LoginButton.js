a7278cdb57319f98d77039a4127a161a7f6b61f2
Alif Rachmawadi <subosito@bukalapak.com>
2020-04-25 12:46:55 +0700

    add osaka login button component

---
+++ packages/snowboard-theme-osaka/osaka/components/LoginButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import qs from "querystringify";
+import { Button } from "baseui/button";
+import { buildAuthorizeUrl } from "snowboard-theme-helper";
+import { useCurrentRoute } from "react-navi";
+import { useStore } from "../lib/store";
+
+export default function({ authOptions, codeChallenge, codeState }) {
+  const route = useCurrentRoute();
+  const [store, dispatch] = useStore();
+
+  const authorizeUrl = buildAuthorizeUrl(authOptions.authorizeUrl, {
+    clientId: authOptions.clientId,
+    callbackUrl: authOptions.callbackUrl,
+    state: codeState,
+    scope: authOptions.scopes,
+    codeChallenge: codeChallenge
+  });
+
+  function handleClick() {
+    dispatch({ type: "setRedirectTo", value: route.url.pathname });
+    window.location = authorizeUrl;
+  }
+
+  return (
+    <Button
+      onClick={handleClick}
+      overrides={{
+        BaseButton: { style: { width: "20%" } }
+      }}
+    >
+      LOGIN
+    </Button>
+  );
+}
