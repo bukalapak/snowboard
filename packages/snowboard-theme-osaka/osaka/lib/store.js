bf3be508046046160a8cbf5726b049921d7bc151
Alif Rachmawadi <subosito@bukalapak.com>
2020-04-25 12:27:56 +0700

    add osaka store provider

---
+++ packages/snowboard-theme-osaka/osaka/lib/store.js
@@ -0,0 +1,38 @@
+import React, { useReducer, useContext } from "react";
+import { getEnv, setEnv, setToken, getToken } from "snowboard-theme-helper";
+import { basePath } from "./helper";
+
+const StoreContext = React.createContext();
+
+const initialState = {
+  env: getEnv(),
+  token: getToken(),
+  redirectTo: basePath
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setEnv":
+      setEnv(action.env);
+      return { ...state, env: action.env };
+    case "setRedirectTo":
+      return { ...state, redirectTo: action.redirectTo };
+    case "setToken":
+      setToken(action.env, action.token);
+      return { ...state, env: action.env, token: action.token };
+    default:
+      throw new Error("unexpected action");
+  }
+};
+
+export const StoreProvider = ({ children }) => {
+  const value = useReducer(reducer, initialState);
+
+  return (
+    <StoreContext.Provider value={value}>{children}</StoreContext.Provider>
+  );
+};
+
+export const useStore = () => {
+  return useContext(StoreContext);
+};
