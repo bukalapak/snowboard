8a3828d68fcb32421798f110a7400e74529e197d
Alif Rachmawadi <subosito@bukalapak.com>
2020-04-25 13:36:13 +0700

    add osaka header panel component

---
+++ packages/snowboard-theme-osaka/osaka/components/panels/HeaderPanel.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Block } from "baseui/block";
+import FieldSwitch from "../FieldSwitch";
+
+export default function({ headers, onChange, setChecked }) {
+  if (headers.length == 0) {
+    return (
+      <Block
+        overrides={{
+          Block: {
+            style: { textAlign: "center" }
+          }
+        }}
+      >
+        <em>No configurable headers.</em>
+      </Block>
+    );
+  }
+
+  return (
+    <Block marginTop="scale400">
+      {headers.map(header => {
+        return (
+          <FieldSwitch
+            key={header.name}
+            name={header.name}
+            value={header.example}
+            used={header.used}
+            required={header.required}
+            rounded={true}
+            onChange={onChange}
+            setChecked={setChecked}
+          />
+        );
+      })}
+    </Block>
+  );
+}
